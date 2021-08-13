6cc381974519fa7ebbf1f998a8c3e2f212ba8df2
LionelM17 <52699443+LionelM17@users.noreply.github.com>
2021-08-13 17:10:57 +0800

    put code in this env

---
+++ DDPG.py
@@ -0,0 +1,176 @@
+import gym
+import copy
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from Agent.BaseAgent import BaseAgent
+
+class ActorNet(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(ActorNet, self).__init__()
+
+        self.fc1 = nn.Linear(state_dim, 1024)
+        self.fc1.weight.data.normal_(0, 0.1)
+        self.fc2 = nn.Linear(1024, 256)
+        self.fc2.weight.data.normal_(0, 0.1)
+        self.out = nn.Linear(256, action_dim)
+        self.out.weight.data.normal_(0, 0.1)
+
+        # self.l1 = nn.Linear(state_dim, 64)
+        # self.l2 = nn.Linear(64, action_dim)
+
+    def forward(self, state):
+        q = F.relu(self.fc1(state))
+        q = F.relu(self.fc2(q))
+        q = self.out(q)
+        # #q = torch.tanh(q)
+        # q = torch.max(q)
+        # action = q * 0.005 + 0.059
+        return q
+
+
+class CriticNet(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(CriticNet, self).__init__()
+        self.fcs = nn.Linear(state_dim, 1024)
+        self.fcs.weight.data.normal_(0, 0.1)
+        self.fca = nn.Linear(action_dim, 1024)
+        self.fca.weight.data.normal_(0, 0.1)
+        self.out = nn.Linear(1024, 1)
+        self.out.weight.data.normal_(0, 0.1)
+
+    def forward(self, state, action):
+        x = self.fcs(state)
+        y = self.fca(action)
+        action_value = self.out(F.relu(x + y))
+        return action_value
+
+class DDPG_Agent(BaseAgent):
+    def __init__(
+            self,
+            num_gen,
+            action_dim,
+            state_dim,
+            gamma = 0.99,
+            optimizer = "Adam",
+            optimizer_paoameters = {},
+            tau = 0.001,
+            initial_eps = 1.0,
+            end_eps = 0.001,
+            eps_decay = 5e3,
+        ):
+
+        BaseAgent.__init__(num_gen)
+
+        self.action_dim = action_dim
+        self.state_dim = state_dim
+        self.state_shape = (-1, state_dim)
+
+        self.gamma = gamma
+        self.tau = tau
+        self.initial_eps = initial_eps
+        self.end_eps = end_eps
+        self.eps_decay = eps_decay
+
+        self.actor = ActorNet(state_dim, action_dim)
+        self.actor_target = copy.deepcopy(self.actor)
+        self.critic = CriticNet(state_dim, action_dim)
+        self.critic_target = copy.deepcopy(self.critic)
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=self.tau)
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=self.tau)
+
+    def act(self, obs, reward, done = False):
+        return action
+
+    def copy_target_update(self):
+        # Softly update the target networks
+        for x in self.actor_target.state_dict().keys():
+            eval('self.actor_target.' + x + '.data.mul_((1-self.tau))')
+            eval('self.actor_target.' + x + '.data.add_(self.tau*self.actor.' + x + '.data)')
+        for x in self.critic_target.state_dict().keys():
+            eval('self.critic_target.' + x + '.data.mul_((1-self.tau))')
+            eval('self.critic_target.' + x + '.data.add_(self.tau*self.critic.' + x + '.data)')
+
+    def train(self, replay_buffer):
+        # Sample replay buffer
+        state, action, next_state, reward, done = replay_buffer.sample()
+
+        # Make action and evaluate its action values
+        action_out = self.actor(state)
+        Q = self.critic(state, action_out)
+        actor_loss = -torch.mean(Q)
+
+        # Optimize the actor network
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+
+        # Compute the target Q value using the information of next state
+        action_target = self.actor_target(next_state)
+        Q_tmp = self.critic_target(next_state, action_target)
+        Q_target = reward + self.gamma * Q_tmp
+
+        # Compute the current Q value and the loss
+        Q_current = self.critic(state, action)
+        critic_loss = nn.MSELoss()(Q_target, Q_current)
+
+        # Optimize the critic network
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
+        return {
+            'Q': Q_current.mean().detach().cpu().numpy(),
+            'Q_loss': critic_loss.mean().detach().cpu().numpy(),
+        }
+
+    def save(self, filename):
+        torch.save(self.actor.state_dict(), filename + "_DDPG_actor")
+        torch.save(self.actor_optimizer.state_dict(), filename + "DDPG_actor_optimizer")
+        torch.save(self.critic.state_dict(), filename + "_DDPG_critic")
+        torch.save(self.critic_optimizer.state_dict(), filename + "_DDPG_critic_optimizer")
+
+    def load(self, filename):
+        self.actor.load_state_dict(torch.load(filename + "_DDPG_actor"))
+        self.actor_optimizer.load_state_dict(torch.load(filename + "_DDPG_actor_optimizer"))
+        self.critic.load_state_dict(torch.load(filename + "_DDPG_critic"))
+        self.critic_optimizer.load_state_dict(torch.load(filename + "_DDPG_critic_optimizer"))
+
+class StandardBuffer(object):
+    def __init__(self, state_dim, num_actions, batch_size, buffer_size, device):
+        self.batch_size = batch_size
+        self.max_size = int(buffer_size)
+        self.buffer_size = int(buffer_size)
+        self.device = device
+
+        self.ptr = 0
+        self.crt_size = 0
+
+        self.state = np.zeros((self.max_size, state_dim))
+        self.action = np.zeros((self.max_size, num_actions))
+        self.next_state = np.array(self.state)
+        self.reward = np.zeros((self.max_size, 1))
+        self.not_done = np.zeros((self.max_size, 1))
+
+    def add(self, state, action, next_state, reward, done, episode_done, episode_start):
+        self.state[self.ptr] = state
+        self.action[self.ptr] = action
+        self.next_state[self.ptr] = next_state
+        self.reward[self.ptr] = reward
+        self.not_done[self.ptr] = 1. - done
+
+        self.ptr = (self.ptr + 1) % self.max_size
+        self.crt_size = min(self.crt_size + 1, self.max_size)
+
+    def sample(self):
+        ind = np.random.randint(
+            max(0, self.crt_size - self.buffer_size), self.crt_size, size=self.batch_size)
+        return (
+            torch.FloatTensor(self.state[ind]).to(self.device),
+            torch.FloatTensor(self.action[ind]).to(self.device),
+            torch.FloatTensor(self.next_state[ind]).to(self.device),
+            torch.FloatTensor(self.reward[ind]).to(self.device),
+            torch.FloatTensor(self.not_done[ind]).to(self.device)
+        )
---
+++ main.py
@@ -0,0 +1,151 @@
+﻿# -*- coding: UTF-8 -*-
+import numpy as np
+import torch
+import copy
+
+from Agent.DoNothingAgent import DoNothingAgent
+from Agent.RandomAgent import RandomAgent
+from Environment.base_env import Environment
+from utilize.settings import settings
+import DDPG
+
+def run_task(my_agent):
+
+    for episode in range(max_episode):
+        print('------ episode ', episode)
+        env = Environment(settings, "EPRIReward")
+        print('------ reset ')
+        obs = env.reset()
+
+        reward = 0.0
+        done = False
+        
+        # while not done:
+        for timestep in range(max_timestep):
+            ids = [i for i,x in enumerate(obs.rho) if x > 1.0]
+            # print("overflow rho: ", [obs.rho[i] for i in ids])    
+            print('------ step ', timestep)
+            action = my_agent.act(obs, reward, done)
+            # print("adjust_gen_p: ", action['adjust_gen_p'])
+            # print("adjust_gen_v: ", action['adjust_gen_v'])
+            obs, reward, done, info = env.step(action)
+            print('info:', info)
+            if done:
+                break
+
+def interact_with_environment(env, replay_buffer, action_dim, state_dim, device, parameters):
+    policy_agent = DDPG.DDPG_Agent(settings.num_gen, action_dim, state_dim)
+
+    state, done = env.reset(), False
+    episode_start = True
+    episode_reward = 0
+    episode_timesteps = 0
+    episode_num = 0
+
+    eps = policy_agent.initial_eps
+
+    # interact with the enviroment for max_timesteps
+    for t in range(parameters.max_timesteps):
+        episode_timesteps += 1
+        if t < parameters["start_timesteps"]:
+            action = RandomAgent.act(state)
+        else:
+            action =  policy_agent.select_action(np.array(state), eps)
+        #print(action)
+        next_state, reward, done, info = env.step(action)
+        episode_reward += reward
+        done_float = float(done)
+
+        # add to replaybuffer
+        replay_buffer.add(state, action, next_state, reward, done_float, done, episode_start)
+        state = copy.copy(next_state)
+        episode_start = False
+
+        # Train agent after collecting sufficient data
+        if t >= parameters["start_timesteps"] and (t+1) % parameters["train_freq"] == 0:
+            info = policy_agent.train(replay_buffer)
+            # for k,v in info.items():
+            #     summary.add_scalar(k, v, t)
+
+        if (t % 16 == 0):
+            policy_agent.copy_target_update()
+        # input_data = input[t:t + 200]
+        # policy, episode_reward = MPC_control(t, env, input_data, parameters["control_circle"], policy, state,
+        #                                      replay_buffer, 5)
+        # action = policy.select_action(np.array(state), eps)
+        # next_state, reward, done, P_loss = env.step(action)
+        # state = copy.copy(next_state)
+        # info = policy.train(replay_buffer)
+        # for k, v in info.items():
+        #     summary.add_scalar(k, v, t)
+        episode_start = False
+
+        if done:
+            print(
+                f"Total T: {t + 1} Episode Num: {episode_num + 1} Episode T: {episode_timesteps} Reward: {episode_reward:.3f} average_Reward:{episode_reward / episode_timesteps:.3f}")
+            summary.add_scalar('reward', episode_reward, t)
+            summary.add_scalar('average_reward', episode_reward / episode_timesteps, t)
+            summary.add_scalar('episode_timesteps', episode_timesteps, t)
+            # summary.add_scalar('P_loss', P_loss, t)
+            # Reset environment
+            state, done = env.reset(), False
+            episode_start = True
+            episode_reward = 0
+            episode_timesteps = 0
+            episode_num += 1
+            if eps > policy_agent.end_eps:
+                eps *= policy_agent.eps_decay
+            else:
+                eps = policy_agent.end_eps
+            #print(eps)
+
+    return policy_agent
+
+if __name__ == "__main__":
+    # max_timestep = 10  # 最大时间步数
+    # max_episode = 1  # 回合数
+    #
+    # my_agent = RandomAgent(settings.num_gen)
+    #
+    # run_task(my_agent)
+    parameters = {
+        "start_timesteps": 2,
+        "initial_eps": 0.9,
+        "end_eps": 0.001,
+        "eps_decay": 0.99,
+        # Evaluation
+        "eval_freq": int(5e2),
+        # Learning
+        "gamma": 0.99,
+        "batch_size": 16,
+        "optimizer": "Adam",
+        "optimizer_parameters": {
+            "lr": 5e-3
+        },
+        "train_freq": 1,
+        "target_update_fre": 1,
+        "tau": 0.001,
+        "control_circle": 3,
+        "input_size": 10,
+        "hidden_size": 35,
+        "num_layers": 3,
+        "seq_len": 3,
+        "lstm_batch_size": 1,
+        "output_size": 1,
+        "max_timestep": 10,
+        "max_episode": 1,
+        "buffer_size": 1e6
+    }
+    # get state dim and action dim
+    state_dim = 10
+
+    env = Environment(settings, "EPRIReward")
+    obs = env.reset()
+    action_dim_p = obs.action_space['adjust_gen_p'].size()
+    action_dim_v = obs.action_space['adjust_gen_v'].size()
+    action_dim = action_dim_p + action_dim_v
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    replay_buffer = DDPG.StandardBuffer(state_dim, action_dim, parameters["batch_size"], parameters["buffer_size"], device)
+    trained_policy_agent = interact_with_environment(env, replay_buffer, action_dim, state_dim, device, parameters)
